ee1b4bf9c8bfe049ff103661819fb9222b110988
Rubens Scotti Junior <128634484+rubensscotti@users.noreply.github.com>
2023-06-10 11:09:31 -0300

    Update 2_corretor.sql
    Alterei para NOT NULL id_seguradora e id_pessoa, pois não é possível alterar a tabela e colocar como PRIMARY KEY campos que sejam NULL.

--- sql/2_corretor.sql
+++ sql/2_corretor.sql
@@ -1,7 +1,7 @@
 -- Tabela "Corretor"
 CREATE TABLE corretor (
-    id_seguradora INT,
-    id_pessoa INT,
+    id_seguradora INT NOT NULL,
+    id_pessoa INT NOT NULL,
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
     FOREIGN KEY (id_seguradora) REFERENCES seguradora(id_seguradora)
 );

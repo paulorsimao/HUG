a28ade18f632bd9920cc98e0c071e894730debeb
stephan-anthony <132624765+stephan-anthony@users.noreply.github.com>
2023-06-13 19:54:20 -0300

    Update 10_pessoa.sql
    Adicionado cpf e data_nasc

--- sql/10_pessoa.sql
+++ sql/10_pessoa.sql
@@ -2,8 +2,10 @@
 CREATE TABLE pessoa (
     id_pessoa SERIAL PRIMARY KEY,
     nome VARCHAR(255),
+    cpf VARCHAR (14) NOT NULL,
     telefone VARCHAR(20),
     email VARCHAR(255),
+    data_nasc DATE,
     rua VARCHAR(100),
     numero INT,
     bairro VARCHAR(50),

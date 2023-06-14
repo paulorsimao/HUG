ca64b84af3f6beb928d92698c8c393c6866fffdd
stephan-anthony <132624765+stephan-anthony@users.noreply.github.com>
2023-06-13 21:00:16 -0300

    Update 1_cliente.sql

--- sql/1_cliente.sql
+++ sql/1_cliente.sql
@@ -1,6 +1,6 @@
 -- Tabela "Cliente"
 CREATE TABLE cliente (
     id_cliente SERIAL PRIMARY KEY,
-    id_pessoa INT,
+    id_pessoa INT NOT NULL,
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa)
 );

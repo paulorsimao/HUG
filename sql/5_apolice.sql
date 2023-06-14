6de3dfb3177aba52ec24653edb97afa090a218d0
stephan-anthony <132624765+stephan-anthony@users.noreply.github.com>
2023-06-13 21:02:22 -0300

    Update 5_apolice.sql

--- sql/5_apolice.sql
+++ sql/5_apolice.sql
@@ -1,11 +1,11 @@
 -- Tabela "Apólice"
 CREATE TABLE apolice (
     id_apolice SERIAL PRIMARY KEY,
-    id_imovel INT,
-    id_cliente INT,
-    data_inicio DATE,
-    data_fim DATE,
-    valor_cobertura DECIMAL(10,2),
+    id_imovel INT NOT NULL,
+    id_cliente INT NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    valor_cobertura DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (id_imovel) REFERENCES imovel(id_imovel),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );

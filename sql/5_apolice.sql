8c5b8d4bbe441322f987a0ea2a2a5eb3753bad1e
Arthurdelucahonorato <126838860+Arthurdelucahonorato@users.noreply.github.com>
2023-06-26 23:28:40 -0300

    Update 5_apolice.sql

--- sql/5_apolice.sql
+++ sql/5_apolice.sql
@@ -2,10 +2,12 @@
 CREATE TABLE apolice (
     id_apolice SERIAL PRIMARY KEY,
     id_imovel INT NOT NULL,
-    id_cliente INT NOT NULL,
+    id_corretor_pessoa NOT NULL,
+    id_corretor_seguradora NOT NULL,
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
     valor_cobertura DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (id_imovel) REFERENCES imovel(id_imovel),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+    FOREIGN KEY (id_corretor_seguradora, id_corretor_pessoa) REFERENCES corretor(id_seguradora, id_pessoa);
 );
